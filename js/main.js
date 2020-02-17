8505927db8b006b9b356f9d1d95e5772a5af8915
Siam Hossain <siam.hossain1994@gmail.com>
2020-02-17 17:14:47 +0600

    Add movie details design

--- js/main.js
+++ js/main.js
@@ -10,6 +10,54 @@ function getMovies(searchText){
     axios.get('http://www.omdbapi.com/?s='+searchText + '&apikey=9be27fce')
         .then((response) => {
             console.log(response);
+            let movies = response.data.Search;
+            let output = '';
+            $.each(movies, (index, movie) => {
+                output += `
+                    <div class="col-md-3">
+                        <div class="well text-center">
+                            <img src="${movie.Poster}">
+                            <h5>${movie.Title}</h5>
+                            <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+                        </div>
+                    </div>
+                `;
+            });
+
+            $('#movies').html(output);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+function movieSelected(id){
+    sessionStorage.setItem('movieId', id);
+    window.location = 'movie.html';
+    return false;
+}
+
+function getMovie(){
+    let movieId = sessionStorage.getItem('movieId');
+
+    axios.get('http://www.omdbapi.com/?i='+movieId + '&apikey=9be27fce')
+        .then((response) => {
+            console.log(response);
+            let movie = response.data;
+
+            let output = `
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="${movie.Poster}" class="thumbnail">
+                    </div>
+                    <div class="col-md-8">
+                        <h2>${movie.Title}
+                    </div>
+                </div>
+            
+                    `;
+
+                $('#movie').html(output);
         })
         .catch((err) => {
             console.log(err);
