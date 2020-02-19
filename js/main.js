621a6db28c1018758e66dc3aeff80d2dd24919fc
Siam Hossain <siam.hossain1994@gmail.com>
2020-02-20 00:37:17 +0600

    Code repetation minimized

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
 });
 
 const proxy = `https://cors-anywhere.herokuapp.com/`;
-const dimage = `image/default_poster.jpg`;
+//const dimage = `image/default_poster.jpg`;
 
 function getMovies(searchText){
     axios.get(`${proxy}http://www.omdbapi.com/?s=`+searchText + `&apikey=9be27fce`)
@@ -15,39 +15,34 @@ function getMovies(searchText){
             console.log(response);
             let movies = response.data.Search;
             let output = '';
+            let src = '';
             $.each(movies, (index, movie) => {
 
                 if(`${movie.Poster}` != "N/A"){
-                    output += `
-                        <div class="col-md-3">
-                            <div class="card card-body text-center">
-                                <img src="${movie.Poster}" class="thumbnail">
-                                <h5>${movie.Title}</h5>
-                                <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
-                            </div>
+                    src = movie.Poster;
+                }else{
+                    src = `image/default_poster.jpg`;
+                }
+
+                output += `
+                    <div class="col-md-3">
+                        <div class="card card-body text-center">
+                            <img src="${src}" class="thumbnail">
+                            <h5>${movie.Title}</h5>
+                            <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
                         </div>
-                    `;
-                }
-
-                else{
-                    output += `
-                        <div class="col-md-3">
-                            <div class="card card-body text-center">
-                                <img src="${dimage}" class="thumbnail">
-                                <h5>${movie.Title}</h5>
-                                <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
-                            </div>
-                        </div>     
-                    `;
-                }
-
+                    </div>
+                `;
             });
+
 
             $('#movies').html(output);
         })
+
         .catch((err) => {
             console.log(err);
         });
+        
 }
 
 function movieSelected(id){
@@ -64,44 +59,18 @@ function getMovie(){
         .then((response) => {
             console.log(response);
             let movie = response.data;
+            let src = '';
 
             if(`${movie.Poster}` != "N/A"){
-                    output = `
-                    <div class="row">
-                        <div class="col-md-4">
-                            <img src="${movie.Poster}" class="thumbnail">
-                        </div>
-                        <div class="col-md-8">
-                            <h2>${movie.Title}</h2>
-                            <ul class="list-group">
-                                <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
-                                <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
-                                <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
-                                <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
-                                <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
-                                <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
-                                <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="card card-body">
-                            <h3>Plot</h3>
-                            ${movie.Plot}
-                            <hr>
-                            <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
-                            <a href="index.html" class="btn btn-default">Go Back To Search</a>
-                        </div>
-                    </div>
-                
-                        `;
+                src = movie.Poster;
+            }else{
+                src = `image/default_poster.jpg`;
             }
-            else{
 
-                output = `
+            output = `
                 <div class="row">
                     <div class="col-md-4">
-                        <img src="${dimage}" class="thumbnail">
+                        <img src="${src}" class="thumbnail">
                     </div>
                     <div class="col-md-8">
                         <h2>${movie.Title}</h2>
@@ -125,16 +94,14 @@ function getMovie(){
                         <a href="index.html" class="btn btn-default">Go Back To Search</a>
                     </div>
                 </div>
-            
-                `;
-                }   
+        
+            `;
 
 
+            $('#movie').html(output);
 
-
-
-                $('#movie').html(output);
         })
+
         .catch((err) => {
             console.log(err);
         });
